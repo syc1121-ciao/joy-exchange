b8ab11fa3ac2ffac75227d117380fd117dc45d51
謝 <b12502009@ntu.edu.tw>
2026-07-22 17:10:39 +0800

    新增 Memo

---
+++ components/Hero.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+export default function Hero() {
+  return (
+    <section className="max-w-7xl mx-auto px-8 py-24">
+
+      <div className="grid grid-cols-2 gap-16 items-center">
+
+        {/* 左邊文字 */}
+
+        <div>
+
+    <p className="uppercase tracking-[0.4em] text-gray-500 text-sm mb-6">
+        Exchange Student 2027
+    </p>
+
+    <h1 className="text-6xl font-serif leading-tight">
+
+        Collect moments,
+        <br />
+
+        not things.
+
+    </h1>
+
+    <p className="mt-8 text-lg text-gray-600 leading-8">
+
+        一場屬於我的交換冒險，
+        <br />
+
+        正在歐洲展開。
+
+    </p>
+
+    <button
+        className="
+        mt-10
+        rounded-full
+        bg-black
+        text-white
+        px-8
+        py-4
+        hover:bg-neutral-700
+        transition
+        "
+    >
+        Explore Journal
+    </button>
+
+</div>
+
+        {/* 右邊圖片 */}
+{/* 右邊圖片 */}
+
+<div className="relative h-[600px] rounded-3xl overflow-hidden shadow-2xl">
+
+    <Image
+        src="/images/cambridge.jpg"
+        alt="Europe"
+        fill
+        className="object-cover"
+    />
+
+</div>
+</div>
+    </section>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,38 @@
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center px-10 py-6 border-b bg-white">
+
+      {/* 左邊 Logo */}
+      <div>
+        <h1 className="text-2xl font-serif italic">
+          Joy's Exchange Adventure
+        </h1>
+      </div>
+
+      {/* 中間選單 */}
+      <ul className="flex gap-10 text-sm uppercase tracking-widest">
+
+        <li>Home</li>
+
+        <li>Journal</li>
+
+        <li>Destinations</li>
+
+        <li>Gallery</li>
+
+        <li>About Me</li>
+
+      </ul>
+
+      {/* 右邊 Icon */}
+      <div className="flex gap-4 text-xl">
+
+        <button>🌙</button>
+
+        <button>🔍</button>
+
+      </div>
+
+    </nav>
+  );
+}
